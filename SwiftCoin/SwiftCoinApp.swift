69c4e4520b7988a1cd0f3c6075ff65f56dd70759
Rumen Guin <rumen2k@gmail.com>
2023-01-13 22:38:24 +0530

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by RUMEN GUIN on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
